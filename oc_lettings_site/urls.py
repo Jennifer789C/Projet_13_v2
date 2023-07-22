ea07e5573e8ce238ffb03beabf8f6237f5640a90
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-07-22 15:40:35 +0200

    déplacement des urls et création des espaces de nom

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
-from lettings.views import letting, lettings_index
 from profiles.views import profile, profiles_index
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', profile, name='profile'),
+    path('lettings/', include("lettings.urls")),
+    path('profiles/', include("profiles.urls")),
     path('admin/', admin.site.urls),
 ]
