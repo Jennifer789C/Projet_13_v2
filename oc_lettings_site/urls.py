3310c8a1d70cbe9454480def08c911d1f3287320
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-07-22 15:54:15 +0200

    renommage des templates et vues index

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from profiles.views import profile, profiles_index
 
 urlpatterns = [
     path('', views.index, name='index'),
