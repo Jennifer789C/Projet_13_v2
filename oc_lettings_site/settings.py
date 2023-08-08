da1edb4e5fa8b901ac8d64f98fea913692d21fa0
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-08-08 12:48:59 +0200

    ajout du travail de déploiement

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -23,8 +23,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "oc-lettings-site.onrender.com"]
 
+CSRF_TRUSTED_ORIGINS = ["https://oc-lettings-site.onrender.com"]
 
 # Application definition
 
